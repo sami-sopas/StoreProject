f75553071fd450b295473cab8bef2cd1d571cefd
sami-sopas <ramirezcuenca@outlook.es>
2023-09-18 16:09:07 -0600

    vista index product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return view('Product.index');
     }
 
     /**
@@ -20,7 +20,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        //vista con form creacion
     }
 
     /**
@@ -28,7 +28,7 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //valida y guardar formulario
     }
 
     /**
