c97007925b1b3ba542b1a1f16a87977734ef46e7
Roberto Castro <robertcr6taal@hotmail.com>
2023-09-24 10:55:06 -0600

    Agregado formato a .php y creado styles.css

--- resources/views/Product/create.blade.php
+++ resources/views/Product/create.blade.php
@@ -4,26 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
 <body>
-    CREANDO PRODUCTO CARAJO
+    <div class="divHeaders" id="divHeaderCreateP">
+        <h1 class="headerH1">CREANDO PRODUCTO CARAJO</h1>
+    </div>
 
-    <br><br>
+    <main>
+        <div class="divMains" id="divMainCreateP">
+            <!--Redirección hacia otras páginas-->
+            <div class="divRedirects" id="divRedirectP">
+                <h2 class="headerRedirects"><a href="{{ route('product.index') }}">Ver lista de Productos</a></h2>
+             </div>     <!--divRedirect end-->
 
-    <form action="{{ route('product.store') }}" method="POST">
-        @csrf
-        <label for="name">Nombre</label>
-        <input type="text" name="name" id="">
-        
-        <br><br>
+            <!-- Titulo de la forma o algo así, centrado-->
+            <div class="divForms" id="divFormP">
+                <form action="{{ route('product.store') }}" method="POST">
+                    @csrf
+                    <div class="divInputs" id="divInputP1">
+                        <label for="name">Nombre
+                        <input type="text" name="name" id="">
+                        </label>
+                    </div>  <!--divInput end-->
+                        <br><br>
+                    <div class="divInputs" id="divInputP2">
+                        <label for="category">Categoria
+                        <input type="text" name="category" id="">
+                        </label>
+                    </div>  <!--divInput end-->
+                        <br><br>
+                    <div class="divButtons" id="divButtonP3">
+                        <button type="submit">Enviar</button>
+                    </div>  <!--divButton end-->
+                </form>
+            </div>  <!--divForm end-->
 
-        <label for="category">Categoria</label>
-        <input type="text" name="category" id="">
 
-        <br><br>
-        <button type="submit">Enviar</button>
-    </form>
-
+        </div>  <!--divMains end-->
+    </main>
 </body>
 </html>
--- resources/views/Product/index.blade.php
+++ resources/views/Product/index.blade.php
@@ -4,21 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    INDEX PRODUCTO
+    <header>
+        <div class="divHeaders" id="divHeaderIndexP">
+            <h1 class="headerH1">INDEX PRODUCTO</h1>
+        </div>  <!--divHeader end-->
+    </header>
 
-    <br>
+            <br>
 
-    <a href="{{ route('product.create') }}">Crear producto carajo</a>
+    <main>
+        <div class="divMains" id="divMainIndexP">
 
-    <h1>Productos</h1>
-    @foreach ($products as $product)
-        
-        Nombre producto: <a href="{{ route('product.show',$product) }}">{{$product->name}} </a> <br>
-        Categoria producto:{{$product->category}}
-        <br><br>
-    @endforeach
+            <div class="divRedirects" id="divRedirect2">    
+               <h2 class="headerRedirects"><a href="{{ route('product.create') }}">Crear producto carajo</a></h2>
+            </div>  <!--divRedirect end-->
+
+            <h2 class="titleh2">Productos</h2>
+            <div class="divLists" id="divList1">
+                @foreach ($products as $product)
+                    <div class="productDiv">
+                        Nombre producto: <a href="{{ route('product.show',$product) }}">{{$product->name}} </a> <br>
+                        Categoria producto:{{$product->category}}
+                        <br><br>
+                    </div>  <!--productDiv end-->
+                    @endforeach
+            </div>  <!--divList end-->
+        </div>  <!--divMain end-->
+    </main>
 </body>
 </html>
