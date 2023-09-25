6990076aa7f572041d5ad2cbe987e77ac005277b
Roberto Castro <robertcr6taal@hotmail.com>
2023-09-24 20:59:06 -0600

    Agregados primeros estilos

--- resources/views/Product/index.blade.php
+++ resources/views/Product/index.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
     <title>Document</title>
 </head>
 <body>
     <header>
         <div class="divHeaders" id="divHeaderIndexP">
-            <h1 class="headerH1">INDEX PRODUCTO</h1>
+            <div class="divHeaderTitles" id="divHeaderTitle1">
+                <h1 class="headerH1">INDEX PRODUCTO</h1>
+            </div><!--divHeaderTitle end-->
         </div>  <!--divHeader end-->
     </header>
 
